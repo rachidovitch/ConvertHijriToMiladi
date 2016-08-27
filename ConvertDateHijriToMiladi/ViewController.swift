56d8e4947c9cd6a6a8d6f46b25b4b3e3ad6abd1c
mac <mac@stefanie.local>
2016-08-28 00:09:55 +0100

    this programme allow you to convert date hijri to Miladi :)

--- ConvertDateHijriToMiladi/ViewController.swift
+++ ConvertDateHijriToMiladi/ViewController.swift
@@ -9,15 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var miladiTextField: UITextField!
+    
+    @IBOutlet weak var hijriLabel: UILabel!
+    
+    @IBOutlet weak var attention: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        hijriLabel.text = ""
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    @IBAction func convertButtonTapped(sender: AnyObject) {
+        
+        if miladiTextField.text != nil && miladiTextField.text != ""{
+        let constant : Double  = ( 33 / 32)
+        
+        var hijri = Double(miladiTextField.text!)! - 622.0
+        
+        
+        hijri *= constant
+        
+        hijriLabel.text = "\(hijri)"
+        
+        miladiTextField.resignFirstResponder()
+        
+            attention.hidden = true
+        }
+        else {
+            attention.hidden = false
+        }
+        
+        
     }
 
 
